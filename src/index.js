3740d35e0c0cfc5588b6997f5c5ead1305b490d6
Thinh Nguyen <miyarin93@gmail.com>
2020-12-26 22:40:24 +0700

    material ui

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import './assets/scss/style.scss';
+import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
